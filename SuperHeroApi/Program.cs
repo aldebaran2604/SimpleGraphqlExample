44fbe379581a33e2e3b7e9aa388f0984d3d1e77e
Luis Jose Padilla Benitez <lupadilla@bansi.com.mx>
2022-11-11 17:06:32 -0600

    Other example of graphql

--- SuperHeroApi/Program.cs
+++ SuperHeroApi/Program.cs
@@ -7,13 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 // Register custom services for the superheroes
 builder.Services.AddScoped<ISuperheroRepository, SuperheroRepository>();
 builder.Services.AddScoped<ISuperpowerRepository, SuperpowerRepository>();
@@ -31,15 +24,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.MapGraphQL();
 
